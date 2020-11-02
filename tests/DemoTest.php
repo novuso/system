72263bdb0e1e81c26f3fbdd04111ae52f3b33b73
John Nickell <john@onlinemeded.org>
2020-11-02 14:25:35 -0600

    saving progress

---
+++ tests/DemoTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Novuso\System\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DemoTest
+ */
+class DemoTest extends TestCase
+{
+    public function test_that_it_runs()
+    {
+        static::assertTrue(true);
+    }
+}
