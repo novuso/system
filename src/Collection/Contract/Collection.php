83aecaefc1e803d6cb84758fe2949cea4c46ec4a
John Nickell <john@onlinemeded.org>
2020-11-09 16:31:38 -0600

    saving progress

--- src/Collection/Contract/Collection.php
+++ src/Collection/Contract/Collection.php
@@ -4,7 +4,6 @@ namespace Novuso\System\Collection\Contract;
 
 use Countable;
 use IteratorAggregate;
-use Traversable;
 
 /**
  * Interface Collection
@@ -15,14 +14,4 @@ interface Collection extends Countable, IteratorAggregate
      * Checks if empty
      */
     public function isEmpty(): bool;
-
-    /**
-     * Retrieves the number of elements
-     */
-    public function count(): int;
-
-    /**
-     * Retrieves an iterator
-     */
-    public function getIterator(): Traversable;
 }
